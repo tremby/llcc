a54d52e41cf54186a493a90eb71a40f9a0047d0b
Bart Nagel <bart@tremby.net>
2018-04-03 22:44:13 -0700

    Challenge day 2

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function gridSize() {
 	const dimensions = gridSizeArray();
 	return `${dimensions[0]} x ${dimensions[1]}`;
 }
+
+function totalCells() {
+	const dimensions = gridSizeArray();
+	return dimensions[0] * dimensions[1];
+}

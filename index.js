8b8c432d10f4ecd33bfb2f53350a5aa6d3757d53
Bart Nagel <bart@tremby.net>
2018-04-04 20:37:18 -0700

    Challenge day 3

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function totalCells() {
 	const dimensions = gridSizeArray();
 	return dimensions[0] * dimensions[1];
 }
+
+function refToIndices(ref) {
+	return [
+		ref.charCodeAt(0) - 'A'.charCodeAt(0),
+		parseInt(ref.substr(1), 10) - 1,
+	];
+}
+
+function cellContents(x, y) {
+	return GRID[y][x];
+}
+
+function lightCell(ref) {
+	return cellContents.apply(this, refToIndices(ref));
+}

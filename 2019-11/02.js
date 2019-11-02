fa1ebdfa17289cd0fc22ed799d6821baf497466d
Bart Nagel <bart@tremby.net>
2019-11-02 12:15:50 -0700

    Add day 2 solution

---
+++ 2019-11/02.js
@@ -0,0 +1,12 @@
+function interviewAnswer(topic) {
+	switch (topic) {
+		case 'arts funding':
+			return "We'll have to get creative!"
+		case 'economy':
+			return "Time is money."
+		case 'transportation':
+			return "It's going to be a long road, so we better get moving."
+		default:
+			return "You're really asking two questions there, and there's no simple answer."
+	}
+}

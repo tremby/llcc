5fbbb47e9f473c48d9a7b959361d2c562d04b15a
Bart Nagel <bart@tremby.net>
2019-11-20 12:38:18 -0800

    Add day 20 solution

---
+++ 2019-11/20.js
@@ -0,0 +1,24 @@
+const PI = 3.14159
+
+function sphereVolume(radius) {
+	return PI * Math.pow(radius, 3) * 4 / 3
+}
+
+function coneVolume(radius, height) {
+	return PI * Math.pow(radius, 2) * height / 3
+}
+
+function prismVolume(height, width, depth) {
+	return height * width * depth
+}
+
+function totalVolume(solids) {
+	return solids.reduce((acc, solid) => {
+		switch (solid.type) {
+			case 'sphere': return acc + sphereVolume(solid.radius)
+			case 'cone': return acc + coneVolume(solid.radius, solid.height)
+			case 'prism': return acc + prismVolume(solid.height, solid.width, solid.depth)
+			default: throw new Error(`Unknown solid type "${solid.type}"`)
+		}
+	}, 0)
+}
